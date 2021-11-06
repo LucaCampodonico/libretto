85e52314dcfc898004642cadde6e7bdbe2c80db5
lucacampodonico <lucacampodonico@mbp-di-luca.homenet.telecomitalia.it>
2021-11-06 10:01:33 +0100

    soluzione 2

--- src/main/java/it/polito/tdp/Libretto_voti/model/libretto.java
+++ src/main/java/it/polito/tdp/Libretto_voti/model/libretto.java
@@ -22,4 +22,45 @@ public class libretto {
 	 }
 	 return s;
  }
+ 
+// public void votiuguali1(int punteggio)
+// {
+////	 voti buttati in console, INUTILE se voglio usare interfaccia 
+// }
+// public String votiuguali2(int punteggio)
+// {
+//	return null;
+////	 una volta sputata la stringa non posso piu gestire ordine
+// }
+// 
+ public List<voto> votiuguali(int punteggio)
+ {
+//	 restituisce voti in modo ordinato ma mostro all'esterno 
+//	 la struttura interna che potrebbe variare nel tempo
+	List<voto> ris=new ArrayList<>();
+	for(voto v:this.voti)
+	{
+		if(v.getVoto()==punteggio)
+		{
+			ris.add(v);
+		}
+	}
+	 return ris;
+ }
+ 
+ public libretto votiUguali(int punteggio)
+ {
+	libretto lib=new libretto();
+	for(voto v : this.voti)
+	{
+		if(v.getVoto()==punteggio)
+		{
+			lib.add(v);
+		}
+	}	
+	return lib; 
+ }
+ 
+
+
 } 
--- src/main/java/it/polito/tdp/Libretto_voti/model/testLibretto.java
+++ src/main/java/it/polito/tdp/Libretto_voti/model/testLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.Libretto_voti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class testLibretto {
 
@@ -10,8 +11,14 @@ public class testLibretto {
     voto voto1=new voto("Analisi 1", 30 ,LocalDate.of(2019, 2, 18));
      libretto.add(voto1);
      libretto.add(new voto("Analisi 2", 30 ,LocalDate.of(2019, 2, 18)));
-     libretto.add(new voto("Analisi 3", 30 ,LocalDate.of(2019, 2, 18)));
+     libretto.add(new voto("Analisi 3", 28 ,LocalDate.of(2019, 2, 18)));
      String s=libretto.toString();
      System.out.println(s);
+     
+    List<voto> venticinque= libretto.votiuguali(30);
+    System.out.println(venticinque);
+    libretto libvc= libretto.votiUguali(28);
+    System.out.println(libvc);
+    		
 	}
 }
